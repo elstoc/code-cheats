afcc9859fc5c0dbcf2d96e602fbabfbab2e3c655
Chris Elston <chris.elston@blueyonder.co.uk>
2022-06-19 22:56:29 +0100

    javascript: initial commit

---
+++ javascript/numbers-and-maths.js
@@ -0,0 +1,33 @@
+/************/
+/* Integers */
+/************/
+
+0           // => 0
+3           // => 3
+100000      // => 100000
+
+0xff        // => 255 (hex)
+0b10101     // => 21 (binary)
+0o377       // => 255 (octal)
+
+/******************/
+/* Floating Point */
+/******************/
+
+3.14
+.3333333
+6.02e23
+1.4738223E-32
+
+/**************/
+/* Separators */
+/**************/
+
+let billion = 1_000_000_000;    // underscore as thousands separator
+let bytes = 0x89_AB_CD_EF;      // as bytes separator
+let bits = 0b0001_1101_0111;    // as nibble separator
+let fraction = 0.123_456_789;   // also works in decimal portion
+
+/*************/
+/* Arithetic */
+/*************/
---
+++ javascript/types-values-variables.js
@@ -0,0 +1,20 @@
+/* Primitive Types (immutable)
+ *  - number
+ *  - string
+ *  - boolean
+ *  - null
+ *  - undefined
+ *  - symbol (ES6)
+ */
+
+/* Objects (mutable)
+ *
+ * JavaScript provides both generic "object" types and some specific types of object
+ *  - array
+ *  - set
+ *  - map
+ *  - typed array
+ *  - RegExp
+ *  - Date
+ *  - Error
+*/
