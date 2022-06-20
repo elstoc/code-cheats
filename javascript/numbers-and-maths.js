8feab6d2b08300c067fefb98c10a5c9cf06bd232
Chris Elston <chris.elston@blueyonder.co.uk>
2022-06-20 10:23:58 +0100

    numbers-and-maths: define common Math functions

--- javascript/numbers-and-maths.js
+++ javascript/numbers-and-maths.js
@@ -31,3 +31,18 @@ let fraction = 0.123_456_789;   // also works in decimal portion
 /*************/
 /* Arithetic */
 /*************/
+Math.pow(2,4)                   // => 16 (2 to the power 4 -- use fractions for roots)
+Math.round(.6)                  // => 1.0 (round to the nearest integer)
+Math.ceil(.6)                   // => 1.0 (round up to an integer)
+Math.floor(.6)                  // => 0.0 (round down to an integer)
+Math.trunc(.6)                  // => 0 (truncate to an integer -- ES6)
+Math.abs(-5)                    // => 5.0
+Math.max(x,y,z)                 // return largest argument
+Math.min(x,y,z)                 // return smallest argument
+Math.random()                   // pseudo-random number 0 <= x < 1.0
+Math.PI
+Math.E
+Math.sqrt(9)                    // => 3
+Math.sin(0)
+Math.log(10)                    // natural logarithm of 10
+Math.log10(100)                 // base 10 logarithm of 100 (ES6)
