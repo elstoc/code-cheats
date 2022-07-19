1b1f6fcf61bdd0ddfab05f63fa6d005c9310a46a
Chris Elston <chris.elston@blueyonder.co.uk>
2022-07-19 11:02:10 +0100

    conditionals

---
+++ javascript/conditionals.js
@@ -0,0 +1,21 @@
+/* if...then...else */
+
+// single line if syntax
+if(condition) statement;
+
+// single line if..then..else
+if(condition1)
+    statement1;
+else if(condition2)
+    statement2;
+else
+    statement3;
+
+// with code blocks
+if(condition1) {
+    statements1;
+} else if(condition2) {
+    statements2;
+} else {
+    statements3;
+}
