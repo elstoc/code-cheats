dec571ecb6f6b61a09b3ad4d7bc55d5d13d4e4d8
Chris Elston <chris.elston@blueyonder.co.uk>
2022-07-20 10:59:33 +0100

    finish objects section

--- javascript/objects.js
+++ javascript/objects.js
@@ -40,6 +40,13 @@ object["z"] = 3                 // write using array notation
 delete object.x                 // delete using dot notation
 delete object["x"]              // delete using array notation
 
+/******************************/
+/* Assignment Shorthand (ES6) */
+/******************************/
+
+let x = 1, y = 2
+let point = { x, y }            // this is equivalent to point = { x:x, y:y }
+
 /************************/
 /* Object Decomposition */
 /************************/
@@ -99,14 +106,77 @@ for(let prop in obj) {
     - Reflect.ownKeys() returns all properties, enumerable or not, symbol or not
 */
 
-/********************************************/
-/* Extending Objects and the Spread Operator*/
-/********************************************/
+/*********************************************/
+/* Extending Objects and the Spread Operator */
+/*********************************************/
 
 let objA = { x: "Ax", y: "Ay", z: "Az" }
 let objB = { y: "By" }
 let objC = { z: "Cz" }
-objA = Object.assign(objA, objB, objC)          // modifies and returns objA = { x: "Ax", y: "By", z: "Cz" }
-                                                // by copying parameters into the first object from subsequent objects
 
-objA = { ...objA, ...objB, ...objC }            // the same operation using the spread operator
+Object.assign(objA, objB, objC, { zz: "zz" } )              // modifies objA = { x: "Ax", y: "By", z: "Cz", zz: "zz" }
+                                                            // by copying parameters into the first object from subsequent objects
+
+newObj = Object.assign({}, objA, objB, objC, { zz: "zz" })  // same thing but returns a new object
+
+newObj = { ...objA, ...objB, ...objC, zz: "zz" }            // a new object again, but using the spread (...) operator
+                                                            // also note the final parameter is not an object here
+
+/********************/
+/* Objects and JSON */
+/********************/
+
+let o = { x: 1, y: { z: [NaN, true, ""] } }
+
+let s = JSON.stringify(o)                               // Convert object to JSON
+s === '{"x":1,"y":{"z":[null,true,""]}}'                // note that JSON cannot represent NaN or Infinity
+JSON.parse(s) === { x: 1, y: { z: [null, true, ""] } }  // Parse JSON to object
+
+/**************************/
+/* Default Object Methods */
+/**************************/
+
+let coordinate = { x: 3000, y: 4000 }
+
+// objects have some default behaviour that may or may not be useful
+
+coordinate.toString() === "[object Object]"                 // default toString method isn't very useful
+Number(coordinate) === NaN                                  // coordinate cannot be cast as a number
+JSON.stringify([coordinate]) === '[{"x":3000,"y":4000}]'    // perhaps we might prefer a different JSON representation
+
+// default behaviour can be overridden with user-defined methods
+
+let coordinate = {
+    x: 3000,
+    y: 4000,
+    toString() { return `(${this.x}, ${this.y})` },                         // you can define your own toString method
+    toLocaleString() {
+        return `(${this.x.toLocaleString()}, ${this.y.toLocaleString()})`   // toLocaleString formats dates/numbers in a locale-specific way
+    },                                                           
+    valueOf() { return Math.hypot(this.x, this.y) },                        // valueOf is used to cast to primitive data types
+    toJSON() { return this.toString() },                                    // toJSON method is used by JSON.stringify
+}                                                                           // Note: you cannot use arrow functions (they can't see "this")
+
+coordinate.toString() === '(3000, 4000)'
+coordinate.toLocaleString() === '(3,000, 4,000)'
+Number(coordinate) === 5000
+JSON.stringify([coordinate]) === '["(3000, 4000)"]'
+
+/***********************/
+/* Getters and Setters */
+/***********************/
+
+let uniqueInteger = {
+    _i: 0,                                  // underscore suggests internal variable (no enforcement)
+    get next() { return this._i++ }         // function to return next value
+    set next(i) {                           // function to increase the value (disallow decrease)
+        if (i > this._i) this._i = i;
+        else throw new Error("this integer has already been used");
+    }
+};
+
+// getters and setters are invoked as if they are normal properties
+
+uniqueInteger.next = 10         // set the value
+uniqueInteger.next              // return next value === 11
+uniqueInteger.next              // return next value === 12
